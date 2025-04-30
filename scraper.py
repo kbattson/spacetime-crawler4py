cc9fc8e8e04bb58924951e1cc14bc3b6525f4eeb
Stephen Clem <noreplydeplo@gmail.com>
2025-04-29 20:14:56 -0700

    kalani changes

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pagedata
 import sqlite3
 
+
 #TODO
 '''
 - implement politeness delays, might be included in base crawler not sure
@@ -102,8 +103,16 @@ def is_valid(url):
                 (netloc == 'today.uci.edu' and path.startswith('/department/information_computer_sciences'))):
                 return False
 
-        if 'account' in netloc:
+        if 'account' in netloc or 'git' in netloc or 'login' in netloc:
             return False
+
+        if netloc.startswith('fano') or netloc.startswith('swiki'):
+            return False
+
+        date_pattern = r'/(19|20)\d{2}[-/](0[1-9]|1[0-2])([-/](0[1-9]|[12][0-9]|3[01]))?/'
+        if re.search(date_pattern, path):
+            return False
+
         
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -119,12 +128,14 @@ def is_valid(url):
             # apk is android something something 
             # pd = pure datak
             # jp = jpeg
-            + r"|mpg|lif|apk|pd|jp)$"
+            # ppsx is mc powerpoint
+            + r"|mpg|lif|apk|pd|jp|img|ppsx|ipynb|bib)$"
             , parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
 
 
 
